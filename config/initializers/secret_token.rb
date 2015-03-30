e21007c10924663bca2e156aee1f33873197156d
jsaini <jsaini@enbake.com>
2015-03-31 00:18:38 +0530

    after first commit having all routes set for invitable user
    after first commit having all routes set for invitable user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvitableUser::Application.config.secret_token = '12baebbc8476268dc121a2216deb19055ed1fb16fa74a7513b0c5e41b72d197549cd34a4d1bb72ade4284f4d720c66e61cde0e6f2eae0439aa6b87bc1ebd2b75'
